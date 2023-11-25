d2309a2be76efbaa2f4dc24ec1597c10c5d7d8c8
Tony van Swet <tonyvanswet@gmail.com>
2023-11-25 15:20:57 +0100

    better favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+          <h1 className="font-sans text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Tony van Swet
           </h1>
         </div>
